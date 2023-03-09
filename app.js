d71c09aeed5c6e111180ee091790eb7056d7064c
nagaramyayellela162 <126769731+nagaramyayellela162@users.noreply.github.com>
2023-03-09 20:42:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose=require("mongoose");
+
+const app = express();
+mongoose.connect("mongodb+srv://ramyasrinivas162:ram123@cluster0.jeouuec.mongodb.net/?retryWrites=true&w=majority/itemsDB");
+mongoose.set('strictQuery',false);
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const itemsSchema=new mongoose.Schema({name:String});
+const Item=mongoose.model("Item",itemsSchema);
+const item1=new Item({name:"Welcome to Todo list"});
+const item2=new Item({name:"Click + symbol to add"});
+const item3=new Item({name:"Hit <-- to delete an item"});
+
+const defaultItems=[item1,item2,item3];
+
+
+
+app.get("/", function(req, res) {
+
+  Item.find({},function(err,foundItems){
+
+    if(foundItems.length === 0){
+
+      Item.insertMany(defaultItems,function(err){
+        if(err){
+          console.log("error");
+        }
+        else{
+          console.log("Success");
+        }
+      });
+      res.redirect("/");
+
+    }
+    else{
+      res.render("list", {listTitle:"Today", newListItems:foundItems});
+ 
+
+    }
+       
+  });
+
+  
+
+});
+
+app.post("/", function(req, res){
+
+  const itemName= req.body.newItem;
+  const add=new Item({
+    name:itemName,
+  })
+  add.save();
+  res.redirect("/")
+
+});
+app.post("/delete",function(req,res){
+  const checkeditemId=req.body.checkbox;
+  Item.deleteOne({_id:checkeditemId},function(err){
+    if(err){
+      console.log("error");
+    }
+    else{
+      console.log("success");
+    }
+  })
+  res.redirect("/");
+})
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
+
